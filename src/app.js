7a14844034eaddf6e008bf16b279a75b87906619
lachlan443 <lachlan.banks1@gmail.com>
2026-05-10 09:35:21 +0000

    fix: add cookie-parser middleware required by csrf-csrf

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const helmet = require('helmet');
 const cors = require('cors');
+const cookieParser = require('cookie-parser');
 const session = require('express-session');
 const SQLiteStore = require('connect-sqlite3')(session);
 const { doubleCsrf } = require('csrf-csrf');
@@ -27,6 +28,7 @@ app.use(cors({
 
 app.use(express.json({ limit: '16kb' }));
 app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
 
 app.use(session({
   store: new SQLiteStore({
